88c4bbe20a2c3fa49cd0e1df82e4b14687085a20
Quang Vuong <macbook@172-15-158-152.lightspeed.ltrkar.sbcglobal.net>
2022-05-08 16:24:17 +0700

    Fix https

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
@@ -17,7 +18,8 @@ import { SignUpComponent } from './sign-up/sign-up.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        HttpClientModule
 
   ],
   providers: [],
