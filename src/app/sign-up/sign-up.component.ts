1f583d7cc93ab4fad431f632eef10601b24c3258
Viet Nguyen <viet.nguyen@team.enouvo.com>
2022-05-26 21:13:11 +0700

    viet push

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -10,51 +10,84 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class SignUpComponent implements OnInit {
   jsonData = {"success":true,"data":{"jwt":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb250cmFjdE5hbWVzcGFjZSI6InRyYWNlYWJpbGl0eXNjLmFzc2V0IiwiY3JlYXRlZEF0IjoiMjAyMi0wNS0wM1QwMTowMjoxNy4wMjFaIiwiY3VycmVudFN0YXRlIjoiSU5JVElBTCIsImlkIjoiNzUiLCJrZXkiOiJcdTAwMDB0cmFjZWFiaWxpdHlzYy5hc3NldFx1MDAwMDc1XHUwMDAwIiwibmFtZSI6InVzZXIwMiIsInBhc3N3b3JkIjoiMTIzNGFiY2QiLCJ0eElEIjoiY2FlYjJjZjJjYWRmYmVkYmFhNzQyNDU2ZWViYjQ4ODFmNWRjNTAyZjJkY2NjY2U0MWY4NDc0ZmJmYWYzZGFlYyIsImlhdCI6MTY1MTU0MDgzNiwiZXhwIjoxNjUxNjI3MjM2fQ.ViXDroAi3sFjDXTdJGlgprkjUkHXZDxy7OCxXgpCYQY","asset":{"contractNamespace":"traceabilitysc.asset","createdAt":"2022-05-03T01:02:17.021Z","currentState":"INITIAL","id":"75","key":"\u0000traceabilitysc.asset\u000075\u0000","name":"user02","password":"1234abcd","txID":"caeb2cf2cadfbedbaa742456eebb4881f5dc502f2dcccce41f8474fbfaf3daec"}},"error":null};
   step: number = 1;
-  loginForm = new FormGroup({
+  accessForm = new FormGroup({
     id: new FormControl(),
     password: new FormControl()
   })
 
-  signUpForm = new FormGroup({
+  createForm = new FormGroup({
     id: new FormControl(),
     userName: new FormControl(),
     password: new FormControl(),
   })
+
+  x = 'string';
+  data;
   constructor(
     public http: HttpClient,
 
   ) { }
 
-  ngOnInit() {
+  ngOnInit() {  
   }
 
-  login() {
-    this.step = 3;
-    this.resetForm();
-  }
-
-  selectSignup() {
+  createAsset() {
     this.step = 2;
     this.resetForm();
   }
 
-  submitSignup() {
-    this.step =1 ;
+  accessAsset() {
+    this.step = 3;
     this.resetForm();
   }
 
-  cancelSignUp() {
-    this.step =1;
+  submitAccessAsset() {
+    this.http.post('http://192.168.230.134/api/v1/asset/login', {
+      id: this.accessForm.get('id')?.value,
+      password: this.accessForm.get('password')?.value
+    }).subscribe(() => {
+      this.step = 1;
+      this.resetForm();
+    })
+  }
+
+  selectSignup() {
+    this.step = 2;
+  }
+
+  submitCreateAsset() {
+    this.http.post('http://192.168.230.134:5000/api/v1/asset', {
+      id: this.createForm.get('id')?.value,
+      name: this.createForm.get('userName')?.value,
+      password: this.createForm.get('password')?.value
+    }).subscribe((res) => {
+      this.data = res
+      this.step = 1;
+      this.resetForm();
+    }, (error) => {
+      console.log(error);
+      this.x = error.error.error
+    })
+    
+  }
+
+  cancelCreateAsset() {
+    this.step = 1;
     this.resetForm();
   }
 
-  logout() {
+  cancelAccessAsset() {
+    this.step = 1;
+    this.resetForm();
+  }
+
+  goback() {
     this.step = 1;
     this.resetForm();
   }
 
   resetForm() {
-    this.loginForm.reset();
-    this.signUpForm.reset();
+    this.accessForm.reset();
+    this.createForm.reset();
   }
 }
